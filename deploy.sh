d3df76c559f77a565b3c3846bd086e0356897e33
daiki-murakami <muranokami1@gmail.com>
2021-03-14 23:56:00 +0900

    sh修正

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 set -Ceu
 
 cd /var/www/projects/rails-test
+git checkout master
+git checkout .
 git pull
+chmod +x deploy.sh
 bundle install --path vendor/bundle
 bundle exec rails assets:precompile RAILS_ENV=production
-sudo systemctl restart nginx
