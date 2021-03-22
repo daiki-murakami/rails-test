5a0d7c6d2c82d95f208d9225101007734cd1d5d4
daiki-murakami <muranokami1@gmail.com>
2021-03-23 01:03:36 +0900

    不要そうな処理を削除

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,4 @@ bundle exec rails assets:precompile RAILS_ENV=production
 
 # unicorn再起動処理
 pid=`cat tmp/pids/unicorn.pid`
-
-echo "starting new server..."
 kill -s USR2 $pid
-sleep 3
-
-echo "stopping old worker..."
-kill -s WINCH $pid
-sleep 3
-
-echo "stopping old master..."
-kill -s QUIT $pid</pre>
