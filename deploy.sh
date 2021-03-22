9667fa26de951affc7189c90e62edf47088640ca
daiki-murakami <muranokami1@gmail.com>
2021-03-23 00:52:11 +0900

    unicorn再起動処理追加

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,14 @@ set -Ceu
 cd /var/www/projects/rails-test
 bundle install --path vendor/bundle
 bundle exec rails assets:precompile RAILS_ENV=production
+
+# unicorn再起動処理
+pid=`cat /tmp/unicorn.pid`
+
+kill -s USR2 $pid
+sleep 3
+
+kill -s WINCH $pid
+sleep 3
+
+kill -s QUIT $pid</pre>
